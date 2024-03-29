d9e1c33ba72878bf56030c96995a9e06294cd4ee
Samuel Mitas <samomitas@gmail.com>
2024-03-29 22:00:53 +0100

    Inline logo

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,20 +1,15 @@
-import Image from 'next/image';
 import { Link } from '../navigation';
 import { I18nConfig } from '@/i18n';
 import { useLocale } from 'next-intl';
+
+import AlExpresLogo from '@/images/al-expres.svg';
 
 export const Header = () => {
   const selectedLocale = useLocale();
   return (
     <header className="p-4 h-20 flex items-center justify-center backdrop-blur-sm bg-black/80 fixed top-0 right-0 left-0 z-30">
       <div className="max-w-6xl w-full flex justify-between">
-        <Image
-          src="/al-expres.svg"
-          alt="Vercel Logo"
-          width={90}
-          height={36}
-          priority
-        />
+        <AlExpresLogo />
         <div>
           {I18nConfig.locales.map((locale) => (
             <Link

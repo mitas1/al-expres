267004c64f46dc1629b7015aed2e445ee59ca93f
Samuel Mitas <samomitas@gmail.com>
2024-04-04 23:04:34 +0200

    Fuck DL semantics

--- src/components/common/description-list/DescriptionList.tsx
+++ src/components/common/description-list/DescriptionList.tsx
@@ -5,22 +5,22 @@ interface DescriptionListProps {
 }
 
 export const DescriptionList: FC<DescriptionListProps> = ({ groups }) => (
-  <dl className="my-4 text-center md:text-left text-sm font-light leading-loose">
+  <div className="my-4 text-center md:text-left text-sm font-light leading-loose">
     {groups.map((group, index) => (
       <div key={index}>
         {group.map(([key, value]) => {
           return (
             <div key={key} className="inline text-xs">
-              <dt className="inline after:content-[':'] after:mr-1 uppercase font-bold">
+              <div className="inline after:content-[':'] after:mr-1 uppercase font-bold">
                 {key}
-              </dt>
-              <dd className="inline after:content-['/'] after:mx-2 after:text-black/50">
+              </div>
+              <div className="inline after:content-['/'] after:mx-2 after:text-black/50">
                 {value}
-              </dd>
+              </div>
             </div>
           );
         })}
       </div>
     ))}
-  </dl>
+  </div>
 );
